daaf5e42a66aff3374026130c7458447ba41ab3a
Vel1ko <veljko.jovic@elev.ga.ntig.se>
2024-04-23 10:16:09 +0200

    mer olika themes

--- Meny.py
+++ Meny.py
@@ -9,16 +9,16 @@ def create_window(theme):
         [sg.Button('Spel 1', size = button_size),sg.Button('Spel 2', size = button_size),sg.Button('Spel 3', size = button_size)],
     ]
 
-    return sg.Window('meny', layout)
+    return sg.Window('meny', layout, size = (600,400))
 
-theme_menu = ['menu',['DarkBlue14', 'DarkGrey6', 'DarkRed']]
-window = create_window('menu')
+theme_menu = ['menu',['DarkBlue14', 'DarkGrey6', 'DarkRed', 'PythonPlus', 'veljko', 'Random']]
+window = create_window('')
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
-    elif event == 'DarkBlue14' or event == 'DarkGrey6' or event == 'DarkRed':
+    elif event == 'DarkBlue14' or event == 'DarkGrey6' or event == 'DarkRed' or event == 'PythonPlus' or event == 'Random' or event == 'veljko':
         window.close()
         window = create_window(event)
 

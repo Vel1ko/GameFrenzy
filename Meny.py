5a8a8e9a9d558a25f05656a7b6b0607e5b55535b
Vel1ko <veljko.jovic@elev.ga.ntig.se>
2024-04-23 10:19:19 +0200

    layout size till original

--- Meny.py
+++ Meny.py
@@ -9,16 +9,16 @@ def create_window(theme):
         [sg.Button('Spel 1', size = button_size),sg.Button('Spel 2', size = button_size),sg.Button('Spel 3', size = button_size)],
     ]
 
-    return sg.Window('meny', layout, size = (600,400))
+    return sg.Window('meny', layout)
 
-theme_menu = ['menu',['DarkBlue14', 'DarkGrey6', 'DarkRed', 'PythonPlus', 'veljko', 'Random']]
+theme_menu = ['menu',['DarkBlue14', 'DarkGrey6', 'DarkRed', 'PythonPlus', 'Random']]
 window = create_window('')
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
-    elif event == 'DarkBlue14' or event == 'DarkGrey6' or event == 'DarkRed' or event == 'PythonPlus' or event == 'Random' or event == 'veljko':
+    elif event == 'DarkBlue14' or event == 'DarkGrey6' or event == 'DarkRed' or event == 'PythonPlus' or event == 'Random':
         window.close()
         window = create_window(event)
 

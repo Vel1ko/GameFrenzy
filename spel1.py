dc8b799d7c12d81c97c3f3b85c095591de970c35
Vel1ko <veljko.jovic@elev.ga.ntig.se>
2024-04-23 10:54:51 +0200

    Grunderna för Sten sax påse är klara ska va få det att se bra ut

---
+++ spel1.py
@@ -0,0 +1,25 @@
+
+def ssp():
+    import PySimpleGUI as sg
+    import random
+    layout = [ 
+        [sg.Text('Sten ,Sax, påse')],
+        [sg.Button('Rock'), sg.Button('Paper'), sg.Button('Scissors')],
+        [sg.Text(size=(20,1), key='-RESULT-')],
+        [sg.Button('Avsluta')]
+    ]
+
+    window = sg.Window('Sten Sax Påse').Layout(layout)
+
+    while True:
+        event, values = window.Read()
+
+        if event in (None, 'Avsluta'):
+            break
+        elif event in ('Rock', 'Paper', 'Scissors'):
+            user_choice = event.lower()
+            computer_choice = random.choice(['Rock', 'Paper', 'Scissors'])
+            winner = determine_winner(user_choice, computer_choice)
+            window['-RESULT-'].update(winner)
+
+ssp()

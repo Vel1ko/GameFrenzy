1edfb3afd102d122debe64e9837aca8275e36b9d
Vel1ko <veljko.jovic@elev.ga.ntig.se>
2024-04-26 10:36:51 +0200

    bilderna är i spelet

--- spel1.py
+++ spel1.py
@@ -7,7 +7,7 @@ def ssp():
     sg.theme('DarkGrey13')
     layout = [ 
         [sg.Text('Sten Sax Påse', font = 'Franklin 30', justification= 'center')],
-        [sg.Image(r'C:\Users\veljko.jovic\Desktop\Python Uppgifter\GameFrenzy\ROCK_PIC.png', size = (150, 200)),sg.Image(r''),sg.Image(r'')],
+        [sg.Image(r'C:\Users\veljko.jovic\Desktop\Python Uppgifter\GameFrenzy\ROCK_PIC.png', size = (150, 200)),sg.Image(r'C:\Users\veljko.jovic\Desktop\Python Uppgifter\GameFrenzy\paper1.png', size = (150, 200), subsample= 3),sg.Image(r'C:\Users\veljko.jovic\Desktop\Python Uppgifter\GameFrenzy\scissors.png', size = (150, 200))],
         [sg.Button('Rock'), sg.Push(), sg.Push(), sg.Push(), sg.Button('Paper'), sg.Push(), sg.Push(), sg.Push(), sg.Button('Scissors')],
         [sg.Text(size=(20,1), key='-RESULT-')],
         [sg.Button('Avsluta')]

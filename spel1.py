fbf14b746a3f6e488024835f5c15ee1ce12e90cb
Vel1ko <veljko.jovic@elev.ga.ntig.se>
2024-04-29 10:13:13 +0200

    lite code i ssp

--- spel1.py
+++ spel1.py
@@ -9,6 +9,8 @@ def ssp():
         [sg.Text('Sten Sax Påse', font = 'Franklin 30', justification= 'center')],
         [sg.Image(r'C:\Users\veljko.jovic\Desktop\Python Uppgifter\GameFrenzy\ROCK_PIC.png', size = (150, 200)),sg.Image(r'C:\Users\veljko.jovic\Desktop\Python Uppgifter\GameFrenzy\paper1.png', size = (150, 200), subsample= 3),sg.Image(r'C:\Users\veljko.jovic\Desktop\Python Uppgifter\GameFrenzy\scissors.png', size = (150, 200))],
         [sg.Button('Rock'), sg.Push(), sg.Push(), sg.Push(), sg.Button('Paper'), sg.Push(), sg.Push(), sg.Push(), sg.Button('Scissors')],
+        [sg.Push()],
+        [sg.Text('Ditt val'),sg.Text('Robot val')],
         [sg.Text(size=(20,1), key='-RESULT-')],
         [sg.Button('Avsluta')]
     ]
@@ -21,7 +23,7 @@ def ssp():
         if event in (None, 'Avsluta'):
             break
         elif event in ('Rock', 'Paper', 'Scissors'):
-            user_choice = event.lower()
+            user_choice = event
             computer_choice = random.choice(['Rock', 'Paper', 'Scissors'])
             winner = (user_choice, computer_choice)
             window['-RESULT-'].update(winner)

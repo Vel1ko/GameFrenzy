2d7845ad7adbb8b8019e3b4074b4912f879c7fcd
Vel1ko <veljko.jovic@elev.ga.ntig.se>
2024-04-29 11:01:47 +0200

    Senaste

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GameFrenzy
+
+# GameFrenzy är ett mini spel menu med tre olika spel man kan välja mellan och köra i PySimpleGUI.
--- spel2.py
+++ spel2.py
@@ -1,12 +1,17 @@
 import PySimpleGUI as sg
 
-Banana = 400
+FIELD_SIZE = 400
+CELL_NUM = 10
+CELL_SIZE = FIELD_SIZE / CELL_NUM
+
+
+munch = (2,4)
 
 sg.theme('DarkGreen')
 field = sg.Graph(
-    canvas_size = (Banana,Banana),
+    canvas_size = (FIELD_SIZE,FIELD_SIZE),
     graph_bottom_left = (0,0),
-    graph_top_right = (Banana,Banana),
+    graph_top_right = (FIELD_SIZE,FIELD_SIZE),
     background_color= 'Black'
 )
 
